ae714d38a8dc9ce825dee8125e2785b566bd9017
Intelli <pun6sn@gmail.com>
2022-03-06 16:21:59 +0300

    Switched logging from Apache to Bukkit + Main Semantics

--- src/main/java/com/swirb/speedrunai/main/SpeedrunAI.java
+++ src/main/java/com/swirb/speedrunai/main/SpeedrunAI.java
@@ -3,27 +3,29 @@ package com.swirb.speedrunai.main;
 import com.swirb.speedrunai.client.ClientHandler;
 import com.swirb.speedrunai.command.Commands;
 import com.swirb.speedrunai.command.GameMode;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.bukkit.craftbukkit.v1_18_R1.CraftServer;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Arrays;
 
 public class SpeedrunAI extends JavaPlugin {
 
-    private static SpeedrunAI instance;
-    private static ClientHandler clientHandler;
-    public static Logger LOGGER = LogManager.getLogger("SpeedrunAI");
+    private static SpeedrunAI INSTANCE;
+
+    public static SpeedrunAI getInstance() {
+        return INSTANCE;
+    }
+
+    private ClientHandler clientHandler;
+
+    public ClientHandler getClientHandler() {
+        return clientHandler;
+    }
 
     @Override
     public void onEnable() {
-        instance = this;
+        INSTANCE = this;
         clientHandler = new ClientHandler();
-        this.registerEvents();
-        this.registerCommands();
-        ((CraftServer) this.getServer()).getServer().setUsesAuthentication(true);  // offline mode
+        registerCommands();
+        ((CraftServer) this.getServer()).getServer().setUsesAuthentication(true);  //TODO: Move to later in lifecycle
     }
 
     @Override
@@ -31,19 +33,9 @@ public class SpeedrunAI extends JavaPlugin {
         clientHandler.clear();
     }
 
-    private void registerEvents(Listener... listeners) {
-        Arrays.stream(listeners).forEach(listener -> this.getServer().getPluginManager().registerEvents(listener, this));
-    }
-
     private void registerCommands() {
-        this.getCommand("bot").setExecutor(new Commands(clientHandler));
-        this.getCommand("gamemode").setExecutor(new GameMode());
+        getCommand("speedrunai").setExecutor(new Commands(clientHandler));
+        getCommand("gamemode").setExecutor(new GameMode());
     }
 
-    public static SpeedrunAI getInstance() {
-        return instance;
-    }
-    public static ClientHandler getClientHandler() {
-        return clientHandler;
-    }
 }
